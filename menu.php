4b62942eb43ce93fb984d3796ab5d9ea71497ae8
ksdhami <32716134+ksdhami@users.noreply.github.com>
2018-12-07 18:31:34 -0700

    added promoter account page; views/adds events

--- menu.php
+++ menu.php
@@ -5,8 +5,8 @@
 	if( $_SESSION["userType"] == "fan" ): ?>
   <li><a href="index.php">Home</a></li>
   <li><a href="browse_tickets.php">Browse Tickets</a></li>
-  <li><a href="manage_account.php">Account</a></li>
-    <!-- <li><a href="index.php?logout=true">Sign Out</a></li> -->
+  <!-- <li><a href="manage_account.php">Account</a></li> -->
+    <li><a href="index.php?logout=true">Sign Out</a></li>
    <!-- 
     <li><a href="credit_card_form.php">Credit Card Form</a></li>
     <li><a href="credit_card_page.php">Credit Card Page</a></li> 
@@ -15,12 +15,13 @@
 <?php elseif( $_SESSION["userType"] == "promoter"): ?>
   <li><a href="index.php">Home</a></li>
   <li><a href="manage_events.php">Manage Events/Series</a></li>
-  <li><a href="manage_account.php">Account</a></li>
+  <!-- <li><a href="manage_account.php">Account</a></li> -->
   <li><a href="index.php?logout=true">Sign Out</a></li>
 <!--
   <li><a href="event_form.php">Event Form</a></li>
   <li><a href="event_page.php">Event Page</a></li>
   -->
+  <li><a href="account_promoter.php"> Account </a></li>
 <?php else: ?>
   <li><a href="index.php">Home</a></li>
   <li><a href="newUser.php">Create Account</a></li>
