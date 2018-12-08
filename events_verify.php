31a3a3bf8ac55da6c3b2ce886524bb5be159b107
ksdhami <32716134+ksdhami@users.noreply.github.com>
2018-12-07 18:33:27 -0700

    get userid from session;TODO direct to add venue

--- events_verify.php
+++ events_verify.php
@@ -8,6 +8,7 @@
 include 'db_functions.php';
 
   $conn = dbConnect();
+  $userID = $_SESSION["userID"];
 
   $name = $_POST["eventName"];
   $description = $_POST["eventDescription"];
@@ -15,19 +16,19 @@ include 'db_functions.php';
   $duration = $_POST["eventEndHour"] - $_POST["eventStartHour"] - ($_POST["eventEndMinute"] < $_POST["eventStartMinute"]);
 
   $query = "INSERT INTO Event (SeriesID, PromoterID, Name, EventTimestamp, Description, Duration, NumTicketsRemaining, TicketPrice)
-  VALUES ( NULL, " . $_SESSION["userID"] . ", '$name' , '$timeStamp' , '$description' , '$duration', " . $_POST["eventNumTickets"] . ", " . $_POST["eventTicketPrice"] . ")";
+  VALUES ( NULL, {$userID}, '$name' , '$timeStamp' , '$description' , '$duration', " . $_POST["eventNumTickets"] . ", " . $_POST["eventTicketPrice"] . ")";
 
   //$result = mysqli_query($connection, $query);
 
   if(mysqli_query($conn, $query))
   {
-    echo "<p>Credit Card Added Successfully!</p>";
+    echo "<p>Event Added Successfully!</p>";
     // Redirect to this page if successfully inserted data
     header('Location: event_page.php');
 
   }
   else
   {
-    echo "ERROR: Could not execute $query." . mysqli_error($conn);
+    echo "$userID ERROR: Could not execute $query." . mysqli_error($conn);
   }
 ?>
