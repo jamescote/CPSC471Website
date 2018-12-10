33a4e97d93d1745bc81930a7e2e8c4ebc07e8802
syslack_moe@hotmail.com <syslack_moe@hotmail.com>
2018-12-10 11:26:54 -0700

    db update

--- db_functions.php
+++ db_functions.php
@@ -4,7 +4,7 @@
 	// Connect to Database
 	function dbConnect()
 	{
-		$con = mysqli_connect("localhost","root","123456789","cpsc471");
+		$con = mysqli_connect("localhost","root","password","cpsc471");
 		
 		// Handle Connection Errors:
 		if( mysqli_connect_errno($con) )
